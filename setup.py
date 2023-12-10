b15e03e9eece29716a3aefab2c9e7e038c2b4b33
Aditya Raj <115471257+hexronuspi@users.noreply.github.com>
2023-12-10 20:17:01 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eagle',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    # Additional metadata
+    author='Aditya',
+    description='Easy Visualisation of Plots',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hexronuspi/eagle/',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ],
+)
