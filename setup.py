8837f721704381eee0de6d7dcf409d6383b03498
Aditya Raj <115471257+hexronuspi@users.noreply.github.com>
2023-12-12 15:18:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='eagle',
+    name='eagleview',
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        
+        'matplotlib',
+        'Pillow'
     ],
     # Additional metadata
     author='Aditya',
@@ -13,8 +14,4 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/hexronuspi/eagle/',
-    license='MIT',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-    ],
 )
