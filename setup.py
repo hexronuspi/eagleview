bdc94cef192adc3493a2729883995b72e1daad82
Aditya Raj <115471257+hexronuspi@users.noreply.github.com>
2023-12-12 21:42:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,17 @@
-
 from setuptools import setup, find_packages
-import codecs
-import os
 
-VERSION = '0.0.1'
-DESCRIPTION = 'Dataset Visualization tool'
-LONG_DESCRIPTION = 'A package that allows to visualize datasets in minimal line of code'
-
-# Setting up
 setup(
-    name="eagleview",
-    version=0.0.1,
-    author="hexronuspi (Aditya Raj) and contributors",
-    long_description_content_type="text/markdown",
-    long_description=long_description,
+    name='eagleview',
+    version='0.1',
     packages=find_packages(),
-    install_requires=['matplotlib', 'Pillow'],
-    keywords=['python', 'datasets', 'visualize'],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+    install_requires=[
+        'matplotlib',
+        'Pillow'
+    ],
+    # Additional metadata
+    author='Aditya',
+    description='Easy Visualisation of Plots',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hexronuspi/eagle/',
 )
