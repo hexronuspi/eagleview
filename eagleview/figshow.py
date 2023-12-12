cf943f3d29c83d7b1edb409ab07d250e951cdc2d
Aditya Raj <115471257+hexronuspi@users.noreply.github.com>
2023-12-13 04:35:47 +0530

    Update figshow.py

--- eagleview/figshow.py
+++ eagleview/figshow.py
@@ -4,31 +4,25 @@ from PIL import Image
 import math
 
 class ImageMatrix:
-    def __init__(self, folder_path, default_figsize=(10, 10), default_axis='off'):
+    def __init__(self, folder_path, figsize=(10, 10), axis='off'):
         self.folder_path = folder_path
-        self.default_figsize = default_figsize
-        self.default_axis = default_axis
+        self.figsize = figsize
+        self.axis = axis
 
-    def display_image(self, grid_dimensions, figsize=None, axis=None, print_all=True):
-        figsize = figsize if figsize else self.default_figsize
-        axis = axis if axis else self.default_axis
-
+    def display_image(self, grid_dimensions, print_all=True):
         num_columns = grid_dimensions[1]
         images = os.listdir(self.folder_path)
         num_images = len(images)
         num_rows = math.ceil(num_images / num_columns) if print_all else grid_dimensions[0]
 
-        if not print_all:
-            num_images = min(num_images, num_rows * num_columns)
-
-        fig, axs = plt.subplots(num_rows, num_columns, figsize=(figsize[0]*num_columns, figsize[1]*num_rows)) 
+        fig, axs = plt.subplots(num_rows, num_columns, figsize=self.figsize) 
         for i in range(num_rows):
             for j in range(num_columns):
                 index = i * num_columns + j
                 if index < num_images:
                     img = Image.open(os.path.join(self.folder_path, images[index]))
                     axs[i, j].imshow(img)
-                    axs[i, j].axis(axis)  
+                    axs[i, j].axis(self.axis)  
                 else:
                     axs[i, j].axis('off')  
 
