0e1ed4285257f90732c0565fe0a6daaf60ed8cdd
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2024-01-16 14:34:30 +0530

    test phase-stable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eagleview',
+    version='1.2',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'Pillow',
+        'pandas'
+    ],
+    # Additional metadata
+    author='Aditya',
+    description='Easy Visualisation of Plots',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hexronuspi/eagle/',
+)
+    
